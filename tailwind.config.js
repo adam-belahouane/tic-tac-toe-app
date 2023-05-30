04de412a9f77b534b4fda4016cbb7b0e77ac3386
adam-belahouane <adam_belahouane@outlook.com>
2023-05-30 15:30:30 +0100

    code so far

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        blue: "#31C3BD",
+        lightblue: "#65E9E4",
+        orange: "#F2B137",
+        lightorange: "#FFC860",
+        darkNavy: "#1A2A33",
+        semidarknavy: "#1F3641",
+        sliver: "#A8BFC9",
+        sliverhover: "#DBE8ED",
+      },
+      fontFamily: {
+        sans: ["outfit", "sans"]
+      }
+    },
+  },
+  plugins: [],
+}
