b0cf392af33aecb5c1554edd75eb97093f6ae52c
adam-belahouane <adam_belahouane@outlook.com>
2023-06-12 20:15:23 +0100

    reset button logic added, modal component is being used as reset button modal

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,11 +1,11 @@
 'use client'
 import { Cross } from "./Cross";
 import { Nought } from "./Noughts";
-import { useContext } from "react";
+import { useContext, Dispatch } from "react";
 import { GameContext, GameDispatchContext, initialState } from "@/app/layout";
 import { useRouter } from "next/navigation"
 
-export default function Modal({ winner }: { winner: string }) {
+export default function Modal({ winner, isReset, setIsReset }: { winner: string, isReset: boolean, setIsReset: Dispatch<boolean> }) {
     const dispatch = useContext(GameDispatchContext)
     const game = useContext(GameContext)
     const router = useRouter()
@@ -43,15 +43,35 @@ export default function Modal({ winner }: { winner: string }) {
     return (
         <div className="fixed z-1 left-0 top-0 w-full h-full bg-[#00000066]  flex items-center justify-center">
             <div className="w-full bg-semidarknavy h-2/6 flex items-center justify-center flex-col py-6">
-                {winner === "tie" ? <div className="text-sliver font-bold tracking-[1.5px] text-2xl mb-5">ROUND TIED</div> : <><div className="font-bold text-sm tracking-[0.88px] text-sliver">{handleTitle()}</div>
-                    <div className="flex justify-center items-center py-4">
-                        {winner === "x" ? <Cross isGray={false} noughtOrCross='' isSmall={false} /> : <Nought isGray={false} noughtOrCross='' isSmall={false} />}
-                        <div className={`font-bold text-2xl tracking-[1.5px] ml-2 ${winner === "x" ? "text-lightblue" : "text-orange"}`}>TAKES THE ROUND</div>
-                    </div></>}
-                <div>
-                    <button onClick={() => handleQuit()} className="font-bold tracking-[1px] text-[16px] rounded-[10px] bg-sliver p-3 shadow-[0_4px_0_0_#6B8997] hover:bg-sliverhover">QUIT</button>
-                    <button onClick={handleNextRound} className="font-bold tracking-[1px] ml-4 text-[16px] rounded-[10px] bg-orange p-3 shadow-[0_4px_0_0_#CC8B13] hover:bg-lightorange">NEXT ROUND</button>
-                </div>
+                {isReset ?
+                    <>
+                        <div className="text-sliver font-bold tracking-[1.5px] text-2xl mb-5">RESTART GAME?</div>
+                        <div>
+                            <button onClick={() => setIsReset(false)} className="font-bold tracking-[1px] text-[16px] rounded-[10px] bg-sliver p-3 shadow-[0_4px_0_0_#6B8997] hover:bg-sliverhover">NO, CANCEL</button>
+                            <button onClick={() => handleQuit()} className="font-bold tracking-[1px] ml-4 text-[16px] rounded-[10px] bg-orange p-3 shadow-[0_4px_0_0_#CC8B13] hover:bg-lightorange">YES, RESTART</button>
+                        </div>
+                    </> : <>
+                        {
+                            winner === "tie" ?
+                                <div className="text-sliver font-bold tracking-[1.5px] text-2xl mb-5">ROUND TIED</div>
+                                :
+                                <>
+                                    <div className="font-bold text-sm tracking-[0.88px] text-sliver">{handleTitle()}</div>
+                                    <div className="flex justify-center items-center py-4">
+                                        {
+                                            winner === "x" ?
+                                                <Cross isGray={false} noughtOrCross='' isSmall={false} />
+                                                :
+                                                <Nought isGray={false} noughtOrCross='' isSmall={false} />
+                                        }
+                                        <div className={`font-bold text-2xl tracking-[1.5px] ml-2 ${winner === "x" ? "text-lightblue" : "text-orange"}`}>TAKES THE ROUND</div>
+                                    </div>
+                                </>}
+                        <div>
+                            <button onClick={() => handleQuit()} className="font-bold tracking-[1px] text-[16px] rounded-[10px] bg-sliver p-3 shadow-[0_4px_0_0_#6B8997] hover:bg-sliverhover">QUIT</button>
+                            <button onClick={handleNextRound} className="font-bold tracking-[1px] ml-4 text-[16px] rounded-[10px] bg-orange p-3 shadow-[0_4px_0_0_#CC8B13] hover:bg-lightorange">NEXT ROUND</button>
+                        </div>
+                    </>}
             </div>
         </div>
     )
